cccb52d3b466b7f7c3e9eda661067544deb78791
Jeong Kim <joshkim83@outlook.com>
2016-06-06 09:50:10 -0600

    fixing login path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to admin_url
+      redirect_to user_url(@user_id), status: 200
+
     else
       redirect_to login_url, alert:"Invalid Username or Password"
     end
